59d8bb74a0ccf2ffc7021263e545704835689efd
Armando Shala <shalaar3@students.zhaw.ch>
2022-03-06 22:13:59 +0100

    Some stuff

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 // Created by armandoshala on 03.03.22.
 //
 
-//#include "A01/Aufgabe2.c"
+#include "A01/Aufgabe1.h"
 #include "A02/Aufgabe1.h"
 
 int main(void) {
-//    maina2();
-    maina2_1(2022);
+    countWords();
+
 }
